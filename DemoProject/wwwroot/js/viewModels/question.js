d1465f56dc49b96896569684f392ff76808c505e
Trevon Noble <trevon.noble16@gmail.com>
2025-07-31 19:45:05 -0400

    Solved Javascript/Knockout README directions. Set a breakpoint on line 23 and viewed lines until I found bug. A property field of 'data' was not capitalized and therefore was not recognized.

--- DemoProject/wwwroot/js/viewModels/question.js
+++ DemoProject/wwwroot/js/viewModels/question.js
@@ -20,8 +20,12 @@ function InterviewViewModel() {
 
 function QuestionModel(data) {
     // THERE IS A BUG IN THIS FUNCTION
-    this.Text = ko.observable(data.questionText);
+    this.Text = ko.observable(data.QuestionText);
     this.Answer = ko.observable(data.Answer);
     this.showAnswer = ko.observable(false);
+
+    //Adding an observable for hint
+    this.Hint = ko.observable(data.Hint);
+
     this.toggleAnswer = () => this.showAnswer(!this.showAnswer());
 }
