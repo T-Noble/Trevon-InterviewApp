b3222fd83d75298075da4c55cdab74383a94b2b2
Trevon Noble <trevon.noble16@gmail.com>
2025-07-31 23:16:02 -0400

    Fixed the title of the span's as the hint wasn't displaying properly. Added newlines to the Hints as well

--- DemoProject/Controllers/HomeController.cs
+++ DemoProject/Controllers/HomeController.cs
@@ -25,14 +25,14 @@ namespace DemoProject.Controllers
                     Id = 1, 
                     QuestionText = "What is a scoped service?", 
                     Answer = "A service that can be used per request.", 
-                    Hint = "Defines the lifetime of a service. The scope is created and dies with the request." },
+                    Hint = "Defines the lifetime of a service. \nThe scope is created and dies with the request." },
 
                 //Added hint to Question object #2
                 new Question { 
                     Id = 2, 
                     QuestionText = "What does MVC stand for?", 
                     Answer = "Model View Controller" , 
-                    Hint = "M - Handles data and business logic, the user doesn't see this.\nV - What the user sees on the page\nReceives input and directs the flow of data"}
+                    Hint = "M - Handles data and business logic, the user doesn't see this\nV - What the user sees on the page\nC - Receives input and directs the flow of data"}
             };
             return Ok(questions);
         }
