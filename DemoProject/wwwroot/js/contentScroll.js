911c306e2c4e564a2604673616dcad43e70027dd
Trevon Noble <trevon.noble16@gmail.com>
2025-08-10 19:06:30 -0400

    Prev&NextButton CSS updated. Title and description also now change on content change

--- DemoProject/wwwroot/js/contentScroll.js
+++ DemoProject/wwwroot/js/contentScroll.js
@@ -3,15 +3,29 @@ document.addEventListener('DOMContentLoaded', () => {
   const items = Array.from(container.querySelectorAll('.scrollableContent'));
   const prevBtn = document.getElementById('prevBtn');
   const nextBtn = document.getElementById('nextBtn');
-  prevBtn.enable = true;
-  nextBtn.enable = true;
+  let cntTitle   = document.getElementById('cntTitle');
+  let cntDescrip = document.getElementById('cntDescrip');
+
 
   let currentIndex = 0;
+  let contentTitle = [
+    { title: "PreAssessment", description: "An Interview App by Trevon Noble" },
+    { title: "Interview Question 1", description: "Given a string, save the string into a new variable but reversed" },
+    { title: "Interview Question 2", description: "Given an int list, return the list as strings" },
+    { title: "Interview Question 3", description: "Given an object list with properties USMC or NAVY, return only USMC objects" },
+    { title: "Post Interview Gameplan", description: "A plan to further prepare for this role" }
+  ]
 
   //Update buttons every time
   function updateButtons() {
     prevBtn.disabled = currentIndex === 0;
     nextBtn.disabled = currentIndex === items.length - 1;
+  }
+
+  function updateContentAndDescription(){
+    const { title, description } = contentTitle[currentIndex];
+    cntTitle.textContent = title;
+    cntDescrip.textContent = description;
   }
 
   //Navigate to here
@@ -21,11 +35,13 @@ document.addEventListener('DOMContentLoaded', () => {
     //scrolling
     items[currentIndex].scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' });
     updateButtons();
+    updateContentAndDescription();
   }
 
   //Execute
   prevBtn.addEventListener('click', () => goTo(currentIndex - 1));
   nextBtn.addEventListener('click', () => goTo(currentIndex + 1));
   updateButtons();
+  updateContentAndDescription();
 });
 
